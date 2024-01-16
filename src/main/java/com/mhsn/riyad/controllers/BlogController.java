01a8a2e9e223b2ff7b6c5040875cf7b1cd50aae5
SakibRezoan <bsse0611@iit.du.ac.bd>
2024-01-17 00:18:43 +0600

    blog search add

--- src/main/java/com/mhsn/riyad/controllers/BlogController.java
+++ src/main/java/com/mhsn/riyad/controllers/BlogController.java
@@ -3,6 +3,7 @@ package com.mhsn.riyad.controllers;
 import com.mhsn.riyad.entities.Blog;
 import com.mhsn.riyad.repositories.BlogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
--- src/main/java/com/mhsn/riyad/repositories/BlogRepository.java
+++ src/main/java/com/mhsn/riyad/repositories/BlogRepository.java
@@ -2,7 +2,10 @@ package com.mhsn.riyad.repositories;
 
 import com.mhsn.riyad.entities.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BlogRepository extends JpaRepository<Blog, Long> {
