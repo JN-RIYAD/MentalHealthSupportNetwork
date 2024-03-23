9e9f14823ece362a390259cf3af6ea9ca8853588
JN-RIYAD <89318653+JN-RIYAD@users.noreply.github.com>
2024-03-23 12:23:25 +0600

    password changed

--- src/main/java/com/mhsn/riyad/controllers/AuthenticationController.java
+++ src/main/java/com/mhsn/riyad/controllers/AuthenticationController.java
@@ -4,6 +4,8 @@ import com.mhsn.riyad.entities.User;
 import com.mhsn.riyad.services.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -22,6 +25,9 @@ public class AuthenticationController {
     private HttpSession httpSession;
 
     private boolean successToastShown = false;
+
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
 
     @GetMapping("/")
     public String showIndexPage(Model model, HttpSession httpSession) {
@@ -86,6 +92,50 @@ public class AuthenticationController {
             model.addAttribute("error", "Invalid Request...!!!");
             return "registration";
         }
+    }
+
+    @GetMapping("/show-password-change-page")
+    public String showPasswordChangePage(Model model, HttpSession httpSession) {
+        User user = (User) httpSession.getAttribute("user");
+        if (user == null) {
+            model.addAttribute("error", "Login as user to change password");
+            return "login";
+        } else {
+            userService.setRoleInModelAndHttpSession(httpSession, model, user);
+        }
+        return "password-change";
+    }
+
+    @PostMapping("/password-change")
+    public String passwordChange(Model model, HttpSession httpSession, @RequestParam
+    String oldPassword, @RequestParam String newPassword, RedirectAttributes redirectAttributes) {
+        User user = (User) httpSession.getAttribute("user");
+        if (user == null) {
+            model.addAttribute("error", "Login to change password");
+            return "login";
+        } else {
+            userService.setRoleInModelAndHttpSession(httpSession, model, user);
+        }
+
+        String encryptedOldPassword = passwordEncoder.encode(oldPassword);
+        if ( encryptedOldPassword != user.getPassword())
+        {
+            model.addAttribute("error", "Old password not matched");
+            return "password-change";
+        }
+//        if (!newPassword.equals(confirmPassword)) {
+//            model.addAttribute("error", "New password and confirm password do not match");
+//            return "password-change";
+//        }
+
+        String encryptedNewPassword = passwordEncoder.encode(newPassword);
+        user.setPassword(encryptedNewPassword);
+
+        userService.saveUser(user);
+
+        redirectAttributes.addFlashAttribute("success", "Password changed successfully");
+
+        return "password changed";
     }
 
 }
